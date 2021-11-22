e2ad8aabe81fc3cc070fe7decf50018a498b5034
LucasDiScipio <lucasdiscipio@hotmail.com>
2021-11-22 15:53:26 +0100

    t-SNE (task1)

--- tasks.py
+++ tasks.py
@@ -1,4 +1,20 @@
 from utils import *
+from tsne import *
 
 # import des donnees
-data = load_data(path='./dataset.zip')
+images = load_data(path='./dataset')
+
+# t-SNE
+nsamples, nx, ny, nz = images.shape
+
+data = images.reshape((nsamples, nx*ny*nz))
+
+embedded_data = tsne(X=data, perplexity=30.0)
+
+artists = imscatter(embedded_data[:,0], embedded_data[:,1], images, ax=None, zoom=0.1)
+
+# fig, ax = plt.subplots()
+# ax.plot(embedded_data[:,0], embedded_data[:,1])
+# ax.add_artist(artists)
+plt.show()
+
